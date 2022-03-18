d3906617d144e5689d5bac00c4a0a50a3056a97d
H.Halil Olcay <81256755+holcay92@users.noreply.github.com>
2022-03-18 14:11:49 +0300

    function calculates the result by always going to a  bigger number

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import math
 def max_of(number1: int, number2: int):
     if number1 < number2: return number2
     else: return number1
@@ -14,47 +13,31 @@ def is_prime(number):
     return is_prime_number
 
 
-def recursive_sum(tri, i, j):
-
-    if i == len(tri):
+def recursive_sum(tri, i, j, row):
+    if i == row:
         return 0
     else:
-        # print("max of the line:", max_of(recursive_sum(tri, i+1, j),
-        #   recursive_sum(tri, i+1, j+1)), "recursive_sum(tri, i+1, j): ", recursive_sum(tri, i+1, j),
-        #  "recursive_sum(tri, i+1, j+1): ", recursive_sum(tri, i+1, j+1))
-        return tri[i][j] + max_of(recursive_sum(tri, i+1, j), recursive_sum(tri, i+1, j+1))
-
-
-# 1
-# 8 4
-# 2 6 9
-# 8 7 2 4
-
+        return tri[i][j] + max_of(recursive_sum(tri, i+1, j, row), recursive_sum(tri, i+1, j+1, row))
 
 def main():
     with open('input.txt', 'r') as file:
         twoDlist = [[int(x) for x in line.split()] for line in file]
 
     # number of rows
-    row = col = len(twoDlist)
+    row = len(twoDlist)
     maxSum = 0
-# filling array with zeros
-#     iterColumn = 1
-#     for item1 in range(0, row):
-#         for item2 in range(0, row-iterColumn):
-#             twoDlist[item1].append(int(0))
-#         iterColumn = iterColumn + 1
 
-#removing prime numbers
+#removing prime numbers by making them infinity
     for i in range(0, row):
         for j in range(0, i+1):
             if twoDlist[i][j] != None and is_prime(twoDlist[i][j]): twoDlist[i][j] = float('-inf')
 
-    print(twoDlist)
+    #print(twoDlist)
 # calling recursive function
     if twoDlist[0][0] == float('-inf'): return print("There is no path")
-    maxSum = recursive_sum(twoDlist, 0, 0)
-    print("maxSum: ", maxSum)
+
+    maxSum = recursive_sum(twoDlist, 0, 0, row)
+    print(maxSum)
 
 
 if __name__ == "__main__":
